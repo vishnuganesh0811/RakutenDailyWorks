315650f0111eef927bc3d57fdf27d5cc6cb479e9
vishnuganesh0811 <50759358+vishnuganesh0811@users.noreply.github.com>
2022-04-01 16:15:18 +0530

    assesment 01-04-2022

---
+++ web5/public/app.js
@@ -0,0 +1,8 @@
+function save(){
+	const element = document.getElementById('age');
+	console.log(element.value);
+	
+	const elements = document.getElementsByTag('input');
+	console.log(elements);
+
+}
