32e9be64755840cd4e17195eafcc0a4c30e1079e
vishnuganesh0811 <50759358+vishnuganesh0811@users.noreply.github.com>
2022-04-06 11:49:14 +0530

    todays initial commit

--- user-service/src/main/java/com/rakuten/UserController.java
+++ user-service/src/main/java/com/rakuten/UserController.java
@@ -1,9 +1,11 @@
 package com.rakuten;
 
+import java.util.stream.*;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +14,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	List<User> users=new ArrayList<>();
+	List<User> users = new ArrayList<>();
+
 	@PostMapping
 	void saveUser(@RequestBody User user) {
 		System.out.println(user.getName());
 		System.out.println(user.getAge());
 		users.add(user);
 	}
-		@GetMapping
-		List<User> getUsers(){
-			return users;
+
+	@GetMapping
+	List<User> getUsers() {
+		return users;
+	}
+
+	@GetMapping("{name}")
+	User getUserFromName(@PathVariable String name) {
+		System.out.println(name);
+		
+		for(int i=0; i<users.size(); i++) {
+			if(users.get(i).getName().equals(name)) {
+				return users.get(i);
+			}
+		}
+		
+		return null;
 	}
 	
+	@GetMapping("age/{age}")
+	List<User> getUserFromAge(@PathVariable int age) {
+		System.out.println(age);
+		 List<User> filteredUsers = users.stream().filter((user)->user.getAge() == age).collect(Collectors.toList());
+		 return filteredUsers;
+		
+	}
+
 }
