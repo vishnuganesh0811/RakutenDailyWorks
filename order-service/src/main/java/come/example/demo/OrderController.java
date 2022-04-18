a304bc64824c031f71c11cd275821bf4584c41d3
vishnuganesh0811 <50759358+vishnuganesh0811@users.noreply.github.com>
2022-04-18 15:31:07 +0530

    kafka test

--- order-service/src/main/java/come/example/demo/OrderController.java
+++ order-service/src/main/java/come/example/demo/OrderController.java
@@ -1,14 +1,42 @@
 package come.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
-@PostMapping("/order")
-	void createOrder() {
-	System.out.println("test");
+@RequestMapping(value="/order")
+	void speedDelivery() {
+		System.out.println("Speed delivery for this order");
 		
+	}
+	void normalDelivery() {
+		System.out.println("Normal delivery for this ordered");
+		
+	}
+	@PostMapping("/newOrder")
+	void createOrder( @RequestParam("orderId")long orderId, 
+	@RequestParam("name") String name,
+	@RequestParam("price")double price)
+	
+	{
+		if (price>500.00)
+		{
+			speedDelivery();
+		}
+		
+		else {
+			normalDelivery();
+		}
+	}
+	
+	@GetMapping("/order")
+	void getorders()
+	{
+		System.out.println("Get Method called!!!!");
 	}
 
 }
