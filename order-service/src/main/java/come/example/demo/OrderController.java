b079da8a5e37848e2ab7911338d798cca8d16d6b
vishnuganesh0811 <50759358+vishnuganesh0811@users.noreply.github.com>
2022-04-18 16:20:29 +0530

    assignment completed

--- order-service/src/main/java/come/example/demo/OrderController.java
+++ order-service/src/main/java/come/example/demo/OrderController.java
@@ -7,35 +7,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/order")
 public class OrderController {
-@RequestMapping(value="/order")
 	void speedDelivery() {
 		System.out.println("Speed delivery for this order");
-		
+
 	}
+
 	void normalDelivery() {
 		System.out.println("Normal delivery for this ordered");
-		
+
 	}
+
 	@PostMapping("/newOrder")
-	void createOrder( @RequestParam("orderId")long orderId, 
-	@RequestParam("name") String name,
-	@RequestParam("price")double price)
-	
-	{
-		if (price>500.00)
-		{
+	void createOrder(@RequestParam("orderId") long orderId, @RequestParam("name") String name,
+			@RequestParam("price") double price) {
+		if (price > 99) {
 			speedDelivery();
 		}
-		
+
 		else {
 			normalDelivery();
 		}
 	}
-	
+
 	@GetMapping("/order")
-	void getorders()
-	{
+	void getorders() {
 		System.out.println("Get Method called!!!!");
 	}
 
